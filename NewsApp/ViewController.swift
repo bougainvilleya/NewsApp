aed6a7cf6e393d46fe5a4445ea1e6f04d395bf93
Leyla Nyssanbayeva <71069929+bougainvilleya@users.noreply.github.com>
2022-03-09 22:04:12 +0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Leyla Nyssanbayeva on 09.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
